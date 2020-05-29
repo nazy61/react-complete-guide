081f4f3d0ce7f9da08300505a7f47a35c1f64114
Chinaza <ogwo.chinaza@yahoo.com>
2020-05-29 12:01:19 +0100

    created a person component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi i'm a react app</h1>
+      <p>This is really working!</p>
+      <Person />
     </div>
   );
 }
