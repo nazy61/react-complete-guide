850f69975016878785e4a8c24b8f046b8024170a
Chinaza <ogwo.chinaza@yahoo.com>
2020-05-29 20:43:16 +0100

    added state property in the app class and was able to change its value using setState

--- src/App.js
+++ src/App.js
@@ -11,12 +11,22 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Ogwo', age: 20 },
+        { name: 'Mary', age: 26 },
+        { name: 'Chioma', age: 22 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi i'm a react app</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My Hobbies: Games</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
