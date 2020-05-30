7f30f486946eef582b50c8b2558773d81869a118
Chinaza <ogwo.chinaza@yahoo.com>
2020-05-30 21:17:24 +0100

    improved the way of conditionally rendering contents

--- src/App.js
+++ src/App.js
@@ -47,29 +47,34 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'Bethel')}
+            changed={this.nameChangedHandler}> My Hobbies: Games</Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age} />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi i'm a react app</h1>
         <p>This is really working!</p>
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-                click={this.switchNameHandler.bind(this, 'Bethel')}
-                changed={this.nameChangedHandler}> My Hobbies: Games</Person>
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age} />
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-            </div> : null
-        }
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   };
