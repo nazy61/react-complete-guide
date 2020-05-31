9715134f53541a3d4bea91accf5f1697939ae9fd
Chinaza <ogwo.chinaza@yahoo.com>
2020-05-31 09:47:07 +0100

    added radium package to make our javascript styles support pseudo styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -46,7 +47,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -66,6 +71,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -89,4 +98,4 @@ class App extends Component {
   };
 }
 
-export default App;
+export default Radium(App);
