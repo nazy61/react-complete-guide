8e487002901314120b77afb1a5d12ec4065c2952
Chinaza <ogwo.chinaza@yahoo.com>
2020-05-31 09:03:21 +0100

    added the nameChangedHandler to the list of Persons component

--- src/App.js
+++ src/App.js
@@ -24,14 +24,19 @@ class App extends Component {
     this.setState({ persons });
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Chinaza', age: 20 },
-        { name: event.target.value, age: 26 },
-        { name: 'Chioma', age: 22 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons });
   }
 
   render() {
@@ -53,7 +58,8 @@ class App extends Component {
               key={person.id}
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />
+              age={person.age}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           )}
         </div>
       );
